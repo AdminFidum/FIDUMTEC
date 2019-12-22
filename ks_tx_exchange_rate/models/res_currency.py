0fa408598acb846edebc8f7aa689eb589b8c127d
AdminFidum <miriam.marinangeli@fidumtec.com>
2019-12-22 11:45:25 -0500

    test

--- ks_tx_exchange_rate/models/res_currency.py
+++ ks_tx_exchange_rate/models/res_currency.py
@@ -14,9 +14,9 @@ class ks_tx_res_currency(models.Model):
     def _get_rates(self, company, date):
         currency_rates = super(ks_tx_res_currency, self)._get_rates(company, date)
         ks_echangerate = self.env.context.get('value_ks_tx_exchange_rate')
-        key = self.env.context.get('currency_id')
-        _logger.info('WATARU ks_echangerate %s key %s',ks_echangerate,key)
-        if ks_echangerate and key:
-            currency_rates[key] = 1.0 / ks_echangerate
+        currency_key = self.env.context.get('currency_id')
+        _logger.info('WATARU ks_echangerate %s currency_key %s',ks_echangerate,currency_key)
+        if ks_echangerate and currency_key:
+            currency_rates[currency_key] = 1.0 / ks_echangerate
 
         return currency_rates
