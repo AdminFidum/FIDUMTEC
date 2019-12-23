bdf646b33ccb90825f17d5be9f2f1b8fb1cd4b77
AdminFidum <miriam.marinangeli@fidumtec.com>
2019-12-22 23:12:28 -0500

    test

--- ks_tx_exchange_rate/models/account_invoice_line.py
+++ ks_tx_exchange_rate/models/account_invoice_line.py
@@ -16,6 +16,4 @@ class ks_tx_account_invoice(models.Model):
     def account_invoice_line(self):
         if self.invoice_id.ks_tx_exchange_rate:
             _logger.info('WATARU line ks_tx_exchange_rate %s currency_id %s',self.ks_tx_exchange_rate,self.currency_id)
-            self.currency_id = self.invoice_id.currency_id
-            self.ks_tx_exchange_rate = self.invoice_id.ks_tx_exchange_rate
             self.ks_tx_local_currency_price = self.quantity * self.price_unit * self.invoice_id.ks_tx_exchange_rate
--- ks_tx_exchange_rate/models/account_payment.py
+++ ks_tx_exchange_rate/models/account_payment.py
@@ -13,9 +13,9 @@ _logger = logging.getLogger(__name__)
 class ks_tx_payment_exchange_rate(models.Model):
 	_inherit = 'account.payment'
 	ks_tx_exchange_rate = fields.Float(string='Exchange Rate')
-	local_currency_price = fields.Monetary(readonly=1)
+	ks_tx_local_currency_price = fields.Monetary(readonly=1)
 
-	@api.onchange('amount','payment_date','currency_id') # if these fields are changed, call method
+	@api.onchange('amount','ks_tx_exchange_rate','payment_date','currency_id') # if these fields are changed, call method
 	def ks_tx_change_date_currency(self):
 		ks_company = self.env['res.company']._company_default_get('account.payment')
 		ks_company_currency = ks_company.currency_id
@@ -24,4 +24,4 @@ class ks_tx_payment_exchange_rate(models.Model):
 		ks_er_date = self.payment_date or fields.Date.today()
 		ks_tx = self.env['account.invoice'].ks_tx_inv_exchange_rate.ks_tx_get_rates(self,ks_er_date,ks_company,ks_from_currency,ks_to_currency)
 		self['ks_tx_exchange_rate'] = ks_tx['ks_er']
-		self.local_currency_price = self.ks_tx_exchange_rate * self.amount
+		self.ks_tx_local_currency_price = self.ks_tx_exchange_rate * self.amount
