6a15ed594ccf2065031774d44a012c2dfeb18dc9
unknown <AdminFidum@github.com>
2020-01-09 16:48:08 -0500

    turnover_country_v18

--- ks_custom_ca_reports/__manifest__.py
+++ ks_custom_ca_reports/__manifest__.py
@@ -1,34 +1,20 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-{        
-    'name': "ks_custom_ca_reports",
-
-    'summary': """
-        """,
-
+{
+    'name': 'Accounting Reports - Custom turnover',
+    'version': '1.1',
+    'category': 'reporting',
     'description': """
-        Helps you handle your accounting needs, if you are not an accountant, we suggest you to install only the Invoicing.
+        A custom report to get turnover by country/partner
     """,
-
-    'author': "Keep It Simple",
-    'website': "http://www.kisforce.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Accounting &amp; Finance',
-    'version': '0.1',
-
-    # always loaded
+    'depends': [
+        'account_reports'
+    ],
     'data': [
         'views/turnover_view.xml',
     ],
-
-    # any module necessary for this one to work correctly
-    'depends': [
-        'account_accountant'
-    ],
-    "demo":[],
-    "installable":True
+    'installable': True,
+    'auto_install': False,
+    'license': 'OEEL-1',
 }
