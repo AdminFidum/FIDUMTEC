292ebac72550c785cfd464839c883405750a3986
unknown <AdminFidum@github.com>
2020-01-09 10:59:34 -0500

    KS_TURNOVER_COUNTRY

--- ks_custom_ca_reports/models/turnover.py
+++ ks_custom_ca_reports/models/turnover.py
@@ -94,6 +94,7 @@ class ReportTurnoverCountry(models.AbstractModel):
                 'columns': [{'name': total}]
                 })
 
+                _logger.info('KS_TURNOVER')
         return lines
 
     def get_report_name(self):
