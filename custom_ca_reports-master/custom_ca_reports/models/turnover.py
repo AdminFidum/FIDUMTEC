f93a969c888968f1ae36c2bb356391889734173b
unknown <AdminFidum@github.com>
2020-01-10 07:57:41 -0500

    turnover_country_v23

--- custom_ca_reports-master/custom_ca_reports/models/turnover.py
+++ custom_ca_reports-master/custom_ca_reports/models/turnover.py
@@ -9,7 +9,7 @@ from odoo.addons.web.controllers.main import clean_action
 
 
 class ReportTurnoverCountry(models.AbstractModel):
-    _name = "account.report.turnover.country"
+    _name = "account_report_turnover_country"
     _description = "Turnover by country/partner"
     _inherit = 'account.report'
 
