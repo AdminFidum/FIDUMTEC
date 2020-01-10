6af2d0f136e447e1aa69f7948c47884fc7f67115
unknown <AdminFidum@github.com>
2020-01-10 11:06:18 -0500

    turnover_co

--- custom_ca_reports/__manifest__.py
+++ custom_ca_reports/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'custom_ca_reports',
+    'name': 'Accounting Reports - Custom turnover',
     'version': '1.1',
     'category': 'reporting',
     'description': """
