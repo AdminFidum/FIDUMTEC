af51d58edfc6a3ab30345148033b0e33c7bd807a
unknown <AdminFidum@github.com>
2020-01-10 16:36:00 -0500

    turnover_co

--- custom_ca_reports/models/turnover.py
+++ custom_ca_reports/models/turnover.py
@@ -95,13 +95,11 @@ class ReportTurnoverCountry(models.AbstractModel):
 
         return lines
 
-    
-
-   # def get_templates(self):
-    #    templates = super(ReportTurnoverCountry, self).get_templates()
-     #   templates['main_template'] = 'custom_ca_reports.template_turnover_report'
-      #  templates['line_template'] = 'custom_ca_reports.line_template_turnover_report'
-       # return templates
+    def get_templates(self):
+        templates = super(ReportTurnoverCountry, self).get_templates()
+        templates['main_template'] = 'custom_ca_reports.template_turnover_report'
+        templates['line_template'] = 'custom_ca_reports.line_template_turnover_report'
+        return templates
 
     def open_invoices(self, options, params):
         partner_id = int(params.get('id').split('_')[0])
