dd9ebcfb9a325eb7aed3a8d9a0b8a32fcec34359
unknown <AdminFidum@github.com>
2020-01-13 08:01:24 -0500

    turnover_co_1301

--- custom_ca_reports/models/turnover.py
+++ custom_ca_reports/models/turnover.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# Part of Odoo. Seek LICENSE file for full copyright and licensing details.
 
 import time
 from odoo import api, models, _
