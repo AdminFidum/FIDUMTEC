9d21e6c11b0c4ded6856bc02e76f7008cdc3993c
unknown <AdminFidum@github.com>
2020-01-20 10:47:50 -0500

    field_list

--- new_field/models/new_field_ks.py
+++ new_field/models/new_field_ks.py
@@ -1,10 +1,10 @@
 from openerp import models, fields, api
 class NewField(models.Model):
     _inherit = 'crm.team'
-    new_user_id = fields.Many2one('crm.team', 'Responsible')
+    team_id_users = fields.Many2one('crm.team', 'Responsible')
 
     @api.multi
     def search_(self):
-        domain = [('team_id.team_type', '=', 'sales')]
+        domain = [('team_id.team_type', '=', 'sales'),'|',('members_ids','=',true),('members_ids.team_type','=','sales')]
         records = self.search(domain)
         return records
