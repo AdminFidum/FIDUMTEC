9bc5ec8b6bcc095a4c3534e48ec09ffb74a25f07
unknown <AdminFidum@github.com>
2020-01-20 09:34:06 -0500

    field-list

--- new_field/models/new_field_ks.py
+++ new_field/models/new_field_ks.py
@@ -1,4 +1,4 @@
 from openerp import models, fields, api
 class NewField(models.Model):
-    _inherit = 'res.users'
-    user_id = fields.Many2one('res.users', 'Responsible')
+    _inherit = 'crm.team'
+    new_user_id = fields.Many2one('crm.team', 'Responsible')
