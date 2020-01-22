41c769e21184ac59c112bfc2a8e1a59f342edf84
unknown <AdminFidum@github.com>
2020-01-22 15:26:05 -0500

    KS_caReport

--- custom_ca_reports/models/turnover.py
+++ custom_ca_reports/models/turnover.py
@@ -100,21 +100,3 @@ class ReportTurnoverCountry(models.AbstractModel):
 
     def get_report_name(self):
         return _('Turnover by country/partner')
-
-    def get_templates(self):
-        templates = super(ReportTurnoverCountry, self).get_templates()
-        templates['main_template'] = 'custom_ca_reports.template_turnover_report'
-        templates['line_template'] = 'custom_ca_reports.line_template_turnover_report'
-        return templates
-
-    def open_invoices(self, options, params):
-        partner_id = int(params.get('id').split('_')[0])
-        [action] = self.env.ref('account.action_invoice_tree1').read()
-        action['context'] = self.env.context
-        action['domain'] = [
-            ('partner_id', '=', partner_id), 
-            ('date', '<=', options.get('date').get('date_to')), 
-            ('date', '>=', options.get('date').get('date_from'))
-        ]
-        action = clean_action(action)
-        return action
