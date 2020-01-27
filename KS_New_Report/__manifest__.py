933680644d3a86710a04e9c9a496da07f1a7246d
unknown <AdminFidum@github.com>
2020-01-27 10:42:05 -0500

    KS_caReport

--- KS_New_Report/__manifest__.py
+++ KS_New_Report/__manifest__.py
@@ -1,30 +1,20 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
-    'name': "ks_new_report",
-
-    'summary': """
-        """,
-
-    'description': """
-        
-    """,
+    'name': 'ks_new_report',
+    'version': '1.1',
     'category': 'reporting',
-
-    'author': "Keep It Simple",
-    'website': "http://www.kisforce.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'description': """
+        A custom report to get turnover by country/partner
+    """,
     'depends': [
         'account_reports'
     ],
     'data': [
         'views/KS_Report_view.xml',
     ],
-    "demo":[],
-    "installable":True
+    'installable': True,
+    'auto_install': False,
+    'license': 'OEEL-1',
 }
