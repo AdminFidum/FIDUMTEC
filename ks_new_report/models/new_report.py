544046e1a3e63f6164b09a4f792e21cd0c6dfd10
unknown <AdminFidum@github.com>
2020-01-27 16:10:59 -0500

    KS_caReport

--- ks_new_report/models/new_report.py
+++ ks_new_report/models/new_report.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
+from odoo import api, models
 
-from odoo import models, fields, api
-from odoo.tools.misc import formatLang
-from odoo.exceptions import UserError
-from odoo.addons.web.controllers.main import clean_action
+class ParticularReport(models.AbstractModel):
+    _name = 'Qweb_report.new_report'
 
+    @api.model
+    def get_report_values(self, docids, data=None):
+        report_obj = self.env['ir.actions.report']
+        report = report_obj._get_report_from_name('module.report_name')
+        docargs = {
+            'doc_ids': docids,
+            'doc_model': report.model,
+            'docs': self,
+        }
+        return docargs
