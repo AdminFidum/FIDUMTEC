c2a31a471bd39138176a5bded9a0e1f95be2e911
unknown <AdminFidum@github.com>
2020-01-27 10:39:23 -0500

    KS_caReport

--- KS_New_Report/__manifest__.py
+++ KS_New_Report/__manifest__.py
@@ -16,7 +16,6 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Customer Relationship Management',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
