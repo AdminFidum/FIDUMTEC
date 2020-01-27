ee58f6ef61762fc80af06927da99b0e054238189
unknown <AdminFidum@github.com>
2020-01-27 10:03:09 -0500

    KS_caReport

---
+++ KS_New_Report/Models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import KS_New_report
---
+++ KS_New_Report/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ks_new_report",
+
+    'summary': """
+        """,
+
+    'description': """
+        
+    """,
+
+    'author': "Keep It Simple",
+    'website': "http://www.kisforce.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Customer Relationship Management',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    #'depends': [
+    #    'crm'
+    #],
+    "demo":[],
+    "installable":True
+}
