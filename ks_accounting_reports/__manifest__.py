a2201c6011540ed14ccaa1c7ea5374fa9f93049f
AdminFidum <miriam.marinangeli@fidumtec.com>
2020-01-27 22:04:02 -0500

    KS Reports Main

--- ks_accounting_reports/__manifest__.py
+++ ks_accounting_reports/__manifest__.py
@@ -1,26 +1,19 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
-    'name': "ks_accounting_reports",
-
-    'summary': """
-        Optimización de los Reportes Contables""",
-
-    'author': "Keep It Simple",
-    'website': "http://kisforce.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        #'views/account_general_ledger.xml',
+    'name': 'KS Accounting Reports',
+    'version': '1.0.0',
+    'category': 'reporting',
+    'description': """
+        A custom report to colombian localization
+    """,
+    'depends': [
+        'account_reports'
     ],
-    # only loaded in demonstration mode
-    'demo': [],
+    'data': [
+        'views/ks_general_ledger.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
 }
