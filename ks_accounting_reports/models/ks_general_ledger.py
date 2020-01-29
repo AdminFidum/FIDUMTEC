435c738cd8892cb15485ae9934ba39580b3c260c
unknown <AdminFidum@github.com>
2020-01-29 15:05:08 -0500

    KS_general_ledger

--- ks_accounting_reports/models/ks_general_ledger.py
+++ ks_accounting_reports/models/ks_general_ledger.py
@@ -207,7 +207,6 @@ class ks_general_ledger(models.AbstractModel):
         unaffected_earnings_type = self.env.ref('account.data_unaffected_earnings')
         for account_id, result in results.items():
             account = self.env['account.account'].browse(account_id)
-            account= account[:2]
             accounts[account] = result
             accounts[account]['initial_bal'] = initial_bal_results.get(account.id, {'balance': 0, 'amount_currency': 0, 'debit': 0, 'credit': 0})
             if account.user_type_id == unaffected_earnings_type and account.company_id not in unaff_earnings_treated_companies:
