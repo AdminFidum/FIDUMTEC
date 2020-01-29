8a2bffa28415494703fa7ddf3fb978f7460463e0
unknown <AdminFidum@github.com>
2020-01-29 14:48:55 -0500

    KS_general_ledger

--- ks_accounting_reports/models/ks_general_ledger.py
+++ ks_accounting_reports/models/ks_general_ledger.py
@@ -206,8 +206,8 @@ class ks_general_ledger(models.AbstractModel):
         unaff_earnings_treated_companies = set()
         unaffected_earnings_type = self.env.ref('account.data_unaffected_earnings')
         for account_id, result in results.items():
-            account = self.env['account.account'].browse(account_id[:'2'])
-            accounts[account] = result
+            account = self.env['account.account'].browse(account_id)
+            accounts[account[:'2']] = result
             accounts[account]['initial_bal'] = initial_bal_results.get(account.id, {'balance': 0, 'amount_currency': 0, 'debit': 0, 'credit': 0})
             if account.user_type_id == unaffected_earnings_type and account.company_id not in unaff_earnings_treated_companies:
                 #add the benefit/loss of previous fiscal year to unaffected earnings accounts
