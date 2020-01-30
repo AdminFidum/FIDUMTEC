209849cefa0c9df053e58679aca765e4213347b1
unknown <AdminFidum@github.com>
2020-01-30 12:24:54 -0500

    KS_general_ledger

--- ks_accounting_reports/models/ks_general_ledger.py
+++ ks_accounting_reports/models/ks_general_ledger.py
@@ -208,6 +208,7 @@ class ks_general_ledger(models.AbstractModel):
         for code, result in results.items():
             account = self.env['account.account'].browse(code)
             accounts[account] = result
+            global realcode
             realcode = account.code[0:2]
             accounts[account]['initial_bal'] = initial_bal_results.get(realcode, {'balance': 0, 'amount_currency': 0, 'debit': 0, 'credit': 0})
             if account.user_type_id == unaffected_earnings_type and account.company_id not in unaff_earnings_treated_companies:
@@ -298,7 +299,7 @@ class ks_general_ledger(models.AbstractModel):
         unfold_all = context.get('print_mode') and len(options.get('unfolded_lines')) == 0
         sum_debit = sum_credit = sum_balance = 0
         for account in sorted_accounts:
-            display_name = account.code + " " + account.name
+            display_name = realcode+" "+account.code + " " + account.name
             if options.get('filter_accounts'):
                 #skip all accounts where both the code and the name don't start with the given filtering string
                 if not any([display_name_part.lower().startswith(options['filter_accounts'].lower()) for display_name_part in display_name.split(' ')]):
