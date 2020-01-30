4e09d1a8dd9cdf72a23c360b0647ff4728aebb23
unknown <AdminFidum@github.com>
2020-01-30 11:39:24 -0500

    KS_general_ledger

--- ks_accounting_reports/models/ks_general_ledger.py
+++ ks_accounting_reports/models/ks_general_ledger.py
@@ -208,7 +208,7 @@ class ks_general_ledger(models.AbstractModel):
         for code, result in results.items():
             accoun = self.env['account.account'].browse(code)
             codesh = accoun.code
-            num = '2'
+            num = "2"
             codesh = code[:num]
             account = self.env['account.account'].browse(codesh)
             accounts[account] = result
