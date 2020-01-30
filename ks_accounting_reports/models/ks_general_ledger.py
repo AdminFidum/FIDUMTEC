aee874cb9693451e36fccc4aeeda507a84421b81
unknown <AdminFidum@github.com>
2020-01-30 14:50:37 -0500

    KS_generaal_ledger

--- ks_accounting_reports/models/ks_general_ledger.py
+++ ks_accounting_reports/models/ks_general_ledger.py
@@ -151,7 +151,7 @@ class ks_general_ledger(models.AbstractModel):
         sql = "%s FROM %s WHERE %s%s"
         if group_by_account:
             sql +=  "GROUP BY \"account_move_line\".balance"
-            sql +=  " or \"account_move_line\".account_id"
+            sql +=  " GROUP BY \"account_move_line\".account_id"
         else:
             sql += " GROUP BY \"account_move_line\".id"
             sql += " ORDER BY MAX(\"account_move_line\".date),\"account_move_line\".id"
