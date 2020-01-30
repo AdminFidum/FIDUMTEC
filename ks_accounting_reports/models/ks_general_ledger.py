6b1de5824ba1e2e3a6ffe7800565fc44fc2c1b0f
unknown <AdminFidum@github.com>
2020-01-30 11:38:09 -0500

    KS_general_ledger

--- ks_accounting_reports/models/ks_general_ledger.py
+++ ks_accounting_reports/models/ks_general_ledger.py
@@ -208,7 +208,8 @@ class ks_general_ledger(models.AbstractModel):
         for code, result in results.items():
             accoun = self.env['account.account'].browse(code)
             codesh = accoun.code
-            codesh = code[:2]
+            num = '2'
+            codesh = code[:num]
             account = self.env['account.account'].browse(codesh)
             accounts[account] = result
             accounts[account]['initial_bal'] = initial_bal_results.get(account.id, {'balance': 0, 'amount_currency': 0, 'debit': 0, 'credit': 0})
