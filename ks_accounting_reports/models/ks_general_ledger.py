468e678cc895ad17c02a623870e4e17d3213786c
unknown <AdminFidum@github.com>
2020-01-31 09:23:09 -0500

    KS_general_ledger

--- ks_accounting_reports/models/ks_general_ledger.py
+++ ks_accounting_reports/models/ks_general_ledger.py
@@ -248,7 +248,7 @@ class ks_general_ledger(models.AbstractModel):
             account = self.env['account.account'].browse(code)
             CodigoCuentas.insert(i,account.code)
             i= i+1
-            _logger.info('RIGOGO - ' + CodigoCuentas[i])
+            _logger.info('RIGOGO - ' + CodigoCuentas[0])
         # For each company, if the unaffected earnings account wasn't in the selection yet: add it manually
         user_currency = self.env.user.company_id.currency_id
         for cid in context.get('company_ids', []):
