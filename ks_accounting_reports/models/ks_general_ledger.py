49333f794e97d74742e09989eb77de290e9349d3
AdminFidum <miriam.marinangeli@fidumtec.com>
2020-02-15 21:31:59 -0500

    log

--- ks_accounting_reports/models/ks_general_ledger.py
+++ ks_accounting_reports/models/ks_general_ledger.py
@@ -288,12 +288,13 @@ class ks_general_ledger(models.AbstractModel):
         ks_lines = {}
         for account in grouped_accounts:
             display_name = account.code + " " + account.name
+            _logger.info('WATARU Reports display_name %s ',display_name)
             try:
                 ks_lines_control[display_name]
             except NameError:
                 ks_lines_control[display_name] = account
                 ks_lines[account] = grouped_accounts[account]
-            
+            _logger.info('WATARU Reports ks_lines_control %s ',ks_lines_control)
             ks_lines[ks_lines_control[display_name]]["lines"] =  ks_lines[ks_lines_control[display_name]]["lines"]+ks_lines[account]["lines"]
             ks_lines[ks_lines_control[display_name]]["total_lines"] = ks_lines[ks_lines_control[display_name]]["total_lines"]+len(ks_lines[account]["lines"])
         return ks_lines
