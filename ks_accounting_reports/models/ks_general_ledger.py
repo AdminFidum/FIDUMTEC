1eeff0a627baf011fc59e62e430d529a0832906a
AdminFidum <miriam.marinangeli@fidumtec.com>
2020-02-15 22:12:43 -0500

    Log

--- ks_accounting_reports/models/ks_general_ledger.py
+++ ks_accounting_reports/models/ks_general_ledger.py
@@ -292,7 +292,6 @@ class ks_general_ledger(models.AbstractModel):
             if not display_name in ks_lines_control:
                 ks_lines_control.update({display_name: account})
                 ks_lines[account] = grouped_accounts[account]
-            _logger.info('WATARU Reports ks_lines_control %s ',ks_lines_control)
             ks_lines[ks_lines_control[display_name]]["balance"] =  ks_lines[ks_lines_control[display_name]]["balance"]+grouped_accounts[account]["balance"]
             ks_lines[ks_lines_control[display_name]]["amount_currency"] =  ks_lines[ks_lines_control[display_name]]["amount_currency"]+grouped_accounts[account]["amount_currency"]
             ks_lines[ks_lines_control[display_name]]["debit"] =  ks_lines[ks_lines_control[display_name]]["debit"]+grouped_accounts[account]["debit"]
@@ -305,7 +304,7 @@ class ks_general_ledger(models.AbstractModel):
                 ks_lines[ks_lines_control[display_name]]["initial_bal"]["amount_currency"] = ks_lines[ks_lines_control[display_name]]["initial_bal"]["amount_currency"]+grouped_accounts[account]["initial_bal"]["amount_currency"]
                 ks_lines[ks_lines_control[display_name]]["initial_bal"]["debit"] = ks_lines[ks_lines_control[display_name]]["initial_bal"]["debit"]+grouped_accounts[account]["initial_bal"]["debit"]
                 ks_lines[ks_lines_control[display_name]]["initial_bal"]["credit"] = ks_lines[ks_lines_control[display_name]]["initial_bal"]["balance"]+grouped_accounts[account]["initial_bal"]["credit"]
-
+        _logger.info('WATARU Reports ks_lines %s ',ks_lines)
         return ks_lines
 
     @api.model
